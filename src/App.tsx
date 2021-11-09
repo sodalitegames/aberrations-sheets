42a061d3ad34b06f393e7658bd64ca1dba96f054
Talon Durrant <talonmdurrant@gmail.com>
2021-11-09 01:05:00 -0500

    getting react-router-dom set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePageLayout from './layouts/home-page-layout';
+import SheetPageLayout from './components/sheet-page-layout';
+import HomePage from './pages/home/home';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route element={<HomePageLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="characters" element={<>characters</>} />
+            <Route path="campaigns" element={<>campaigns</>} />
+            <Route path="settings" element={<>settings</>} />
+            <Route path="profile" element={<>profile</>} />
+            <Route path="privacy" element={<>privacy</>} />
+            <Route path="help" element={<>help</>} />
+            <Route path="about" element={<>about</>} />
+          </Route>
+          <Route path="characters/:charId">
+            <Route index element={<SheetPageLayout />} />
+            <Route path="notes" element={<>notes</>} />
+          </Route>
+          <Route path="campaign/:campId">
+            <Route index element={<SheetPageLayout />} />
+            <Route path="notes" element={<>notes</>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
