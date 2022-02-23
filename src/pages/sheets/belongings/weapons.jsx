53efa6d9b1cece74befd462e823bf4130cad2cd0
Talon Durrant <talonmdurrant@gmail.com>
2022-02-23 18:43:37 -0500

    saving it for future reference

--- src/pages/sheets/belongings/weapons.jsx
+++ src/pages/sheets/belongings/weapons.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 
 import { CheckCircleIcon } from '@heroicons/react/outline';
 
-import { selectCurrentCharacter, selectEquippedWeapons, selectWeapons as selectCharWeapons } from '../../../redux/character/character.selectors';
-import { selectCurrentCampaign, selectWeapons as selectCampWeapons } from '../../../redux/campaign/campaign.selectors';
+import { selectCurrentCharacter, selectEquippedWeapons, selectWeapons as selectCharWeapons, selectArchivedWeapons as selectCharArchivedWeapons } from '../../../redux/character/character.selectors';
+import { selectCurrentCampaign, selectWeapons as selectCampWeapons, selectArchivedWeapons as selectCampArchivedWeapons } from '../../../redux/campaign/campaign.selectors';
 
 import { setModal, setSlideOver } from '../../../redux/app/app.actions';
 import { updateSheetResourceStart } from '../../../redux/sheet/sheet.actions';
@@ -22,9 +23,11 @@ import ListContainer from '../../../components/shared/data/ListContainer';
 import Button from '../../../components/shared/Button';
 
 import DisplayWeapon from '../../../components/sheets/display/DisplayWeapon';
+import Notice from '../../../components/shared/Notice';
 
 const SheetBelongingsWeaponsPage = ({ sheetType }) => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
 
   const charSheet = useSelector(selectCurrentCharacter);
   const campSheet = useSelector(selectCurrentCampaign);
@@ -32,35 +35,109 @@ const SheetBelongingsWeaponsPage = ({ sheetType }) => {
 
   const charWeapons = useSelector(selectCharWeapons);
   const campWeapons = useSelector(selectCampWeapons);
+  const charArchivedWeapons = useSelector(selectCharArchivedWeapons);
+  const campArchivedWeapons = useSelector(selectCampArchivedWeapons);
 
   const [weapon, setWeapon] = useState(null);
   const [id, setId] = useState(null);
 
+  const [weaponsList, setWeaponsList] = useState([]);
+
   useEffect(() => {
-    if (id) {
-      setWeapon(sheetType === 'characters' ? charWeapons.find(weap => weap._id === id) : campWeapons.find(weap => weap._id === id));
-      return;
+    if (sheetType === 'characters') {
+      switch (searchParams.get('show')) {
+        case 'archived':
+          setWeaponsList(charArchivedWeapons);
+          return;
+
+        default:
+          setWeaponsList(charWeapons);
+          return;
+      }
     }
 
-    setWeapon(sheetType === 'characters' ? charWeapons[0] : campWeapons[0]);
-    setId(sheetType === 'characters' ? charWeapons[0]?._id : campWeapons[0]?._id);
-  }, [sheetType, id, charWeapons, campWeapons]);
+    if (sheetType === 'campaigns') {
+      switch (searchParams.get('show')) {
+        case 'archived':
+          setWeaponsList(campArchivedWeapons);
+          return;
+
+        case 'active':
+          setWeaponsList(campWeapons.filter(weap => weap.active));
+          return;
+
+        case 'inactive':
+          setWeaponsList(campWeapons.filter(weap => !weap.active));
+          return;
+
+        default:
+          setWeaponsList(campWeapons);
+          return;
+      }
+    }
+  }, [sheetType, searchParams, charArchivedWeapons, campArchivedWeapons, charWeapons, campWeapons]);
+
+  useEffect(() => {
+    if (weaponsList.length) {
+      if (id) {
+        setWeapon(weaponsList.find(weap => weap._id === id));
+        return;
+      }
+
+      setWeapon(weaponsList[0]);
+      setId(weaponsList[0]._id);
+    }
+  }, [weaponsList, id]);
 
   return (
     <SheetPageContent title="Weapons" columns={4}>
-      {/* All Weapons */}
+      {/* Showing Archived Weapons Notice */}
+      {searchParams.get('show') !== 'current' || searchParams.get('show') !== 'all' ? (
+        <div className="col-span-4">
+          {searchParams.get('show') === 'archived' ? <Notice status="warn" message="You are viewing your archived weapons. To view your current weapons, specify so above." /> : null}
+          {searchParams.get('show') === 'active' ? <Notice status="success" message="You are viewing only your active weapons. To view all of your weapons, specify so above." /> : null}
+          {searchParams.get('show') === 'inactive' ? <Notice status="info" message="You are viewing only your inactive weapons. To view all of your weapons, specify so above." /> : null}
+        </div>
+      ) : null}
+
+      {/* Weapons List */}
       <PanelSection title="Manage Weapons">
         <div className="flow-root mt-2">
           <ListContainer
-            list={sheetType === 'characters' ? charWeapons : campWeapons}
-            button={{ click: () => dispatch(setSlideOver({ type: SlideOverTypes.newWeaponForm, data: { sheetType: sheetType } })), text: 'Add a new Weapon' }}
+            list={
+              sheetType === 'characters'
+                ? searchParams.get('show') === 'archived'
+                  ? charArchivedWeapons
+                  : charWeapons.filter(weap => (searchParams.get('show') === 'active' ? weap.active === true : searchParams.get('show') === 'inactive' ? weap.active === false : true))
+                : searchParams.get('show') === 'archived'
+                ? campArchivedWeapons
+                : campWeapons.filter(weap => (searchParams.get('show') === 'active' ? weap.active === true : searchParams.get('show') === 'inactive' ? weap.active === false : true))
+            }
+            button={
+              searchParams.get('show') !== 'archived' ? { click: () => dispatch(setSlideOver({ type: SlideOverTypes.newWeaponForm, data: { sheetType: sheetType } })), text: 'Add a new Weapon' } : null
+            }
             empty={{
               heading: 'No Weapons',
-              message: 'Get started by creating your first one now',
-              button: { click: () => dispatch(setSlideOver({ type: SlideOverTypes.newWeaponForm, data: { sheetType: sheetType } })), text: 'New Weapon' },
+              message:
+                searchParams.get('show') === 'archived'
+                  ? 'You have no archived weapons'
+                  : searchParams.get('show') === 'active'
+                  ? 'You have no active weapons.'
+                  : searchParams.get('show') === 'inactive'
+                  ? 'You have no inactive weapons'
+                  : 'Get started by creating your first one now',
+              button:
+                searchParams.get('show') !== 'archived' ? { click: () => dispatch(setSlideOver({ type: SlideOverTypes.newWeaponForm, data: { sheetType: sheetType } })), text: 'New Weapon' } : null,
             }}
           >
-            {(sheetType === 'characters' ? charWeapons : campWeapons).map(weapon => (
+            {(sheetType === 'characters'
+              ? searchParams.get('show') === 'archived'
+                ? charArchivedWeapons
+                : charWeapons.filter(weap => (searchParams.get('show') === 'active' ? weap.active === true : searchParams.get('show') === 'inactive' ? weap.active === false : true))
+              : searchParams.get('show') === 'archived'
+              ? campArchivedWeapons
+              : campWeapons.filter(weap => (searchParams.get('show') === 'active' ? weap.active === true : searchParams.get('show') === 'inactive' ? weap.active === false : true))
+            ).map(weapon => (
               <div
                 key={weapon._id}
                 className={classNames('flex justify-between items-center px-2 cursor-pointer', id === weapon._id ? 'bg-gray-100' : 'hover:bg-gray-50')}
@@ -70,15 +147,15 @@ const SheetBelongingsWeaponsPage = ({ sheetType }) => {
 
                 {/* Display if it's a character sheet weapon is equipped */}
                 {sheetType === 'characters' && weapon.equipped ? (
-                  <div className="shrink-0 ml-2" title="Equipped">
-                    <CheckCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                  <div className="ml-2 shrink-0" title="Equipped">
+                    <CheckCircleIcon className="w-6 h-6 text-green-600" aria-hidden="true" />
                   </div>
                 ) : null}
 
                 {/* Display if it's a campaign sheet and weapon is active */}
                 {sheetType === 'campaigns' && weapon.active ? (
-                  <div className="shrink-0 ml-2" title="Active">
-                    <CheckCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                  <div className="ml-2 shrink-0" title="Active">
+                    <CheckCircleIcon className="w-6 h-6 text-green-600" aria-hidden="true" />
                   </div>
                 ) : null}
               </div>
@@ -90,88 +167,117 @@ const SheetBelongingsWeaponsPage = ({ sheetType }) => {
       {/* Selected Weapon */}
       <PanelSection title="Selected Weapon" colSpan={3}>
         {weapon ? (
-          <div className="grid gap-8 grid-cols-3 divide-x divide-gray-200">
+          <div className="grid grid-cols-3 gap-8 divide-x divide-gray-200">
             <div className="col-span-2">
               <DisplayWeapon weapon={weapon} sheetType={sheetType} />
             </div>
 
-            <div className="col-span-1 space-y-4 pl-8">
-              {sheetType === 'characters' ? (
-                <Button dark={weapon.equipped} onClick={() => equipBelonging({ sheetType, sheet: charSheet, belongingType: 'weapons', belonging: weapon, equippedList: equippedWeapons })}>
-                  {weapon.equipped ? 'Unequip' : 'Equip'}
-                </Button>
-              ) : null}
-              {sheetType === 'campaigns' ? (
-                weapon.npcId ? (
-                  <Button
-                    dark
-                    onClick={() =>
-                      dispatch(
-                        updateSheetResourceStart(
-                          sheetType,
-                          campSheet._id,
-                          'weapons',
-                          weapon._id,
-                          { npcId: null },
-                          { notification: { status: 'success', heading: 'Weapon Unassigned', message: `You have successfully unassigned ${weapon.nickname || weapon.name}.` } }
-                        )
-                      )
-                    }
-                  >
-                    Unassign
-                  </Button>
-                ) : (
-                  <Button onClick={() => dispatch(setModal({ type: ModalTypes.assignBelonging, id: weapon._id, data: { type: 'weapons', name: weapon.name } }))}>Assign</Button>
-                )
-              ) : null}
-              {sheetType === 'campaigns' ? (
-                <Button
-                  dark={weapon.active}
-                  onClick={() =>
-                    dispatch(
-                      updateSheetResourceStart(
-                        sheetType,
-                        campSheet._id,
-                        'weapons',
-                        weapon._id,
-                        { active: !weapon.active },
-                        {
-                          notification: {
-                            status: 'success',
-                            heading: `Weapon ${weapon.active ? 'Deactivated' : 'Activated'}`,
-                            message: `You have successfully ${weapon.active ? 'deactivated' : 'activated'} ${weapon.nickname || weapon.name}.`,
-                          },
+            <div className="col-span-1 pl-8 space-y-4">
+              {!weapon.archived ? (
+                <Fragment>
+                  {sheetType === 'characters' ? (
+                    <Button dark={weapon.equipped} onClick={() => equipBelonging({ sheetType, sheet: charSheet, belongingType: 'weapons', belonging: weapon, equippedList: equippedWeapons })}>
+                      {weapon.equipped ? 'Unequip' : 'Equip'}
+                    </Button>
+                  ) : null}
+                  {sheetType === 'campaigns' ? (
+                    weapon.npcId ? (
+                      <Button
+                        dark
+                        onClick={() =>
+                          dispatch(
+                            updateSheetResourceStart(
+                              sheetType,
+                              campSheet._id,
+                              'weapons',
+                              weapon._id,
+                              { npcId: null },
+                              { notification: { status: 'success', heading: 'Weapon Unassigned', message: `You have successfully unassigned ${weapon.nickname || weapon.name}.` } }
+                            )
+                          )
                         }
-                      )
+                      >
+                        Unassign
+                      </Button>
+                    ) : (
+                      <Button onClick={() => dispatch(setModal({ type: ModalTypes.assignBelonging, id: weapon._id, data: { type: 'weapons', name: weapon.name } }))}>Assign</Button>
                     )
-                  }
-                >
-                  {weapon.active ? 'Deactivate' : 'Activate'}
-                </Button>
+                  ) : null}
+                  {sheetType === 'campaigns' ? (
+                    <Button
+                      dark={weapon.active}
+                      onClick={() =>
+                        dispatch(
+                          updateSheetResourceStart(
+                            sheetType,
+                            campSheet._id,
+                            'weapons',
+                            weapon._id,
+                            { active: !weapon.active },
+                            {
+                              notification: {
+                                status: 'success',
+                                heading: `Weapon ${weapon.active ? 'Deactivated' : 'Activated'}`,
+                                message: `You have successfully ${weapon.active ? 'deactivated' : 'activated'} ${weapon.nickname || weapon.name}.`,
+                              },
+                            }
+                          )
+                        )
+                      }
+                    >
+                      {weapon.active ? 'Deactivate' : 'Activate'}
+                    </Button>
+                  ) : null}
+                  <Button onClick={() => dispatch(setSlideOver({ type: SlideOverTypes.newTransactionForm, data: { sheetType, documentType: 'weapons', document: weapon } }))}>Give or Sell</Button>
+                </Fragment>
               ) : null}
-              <Button onClick={() => dispatch(setSlideOver({ type: SlideOverTypes.newTransactionForm, data: { sheetType, documentType: 'weapons', document: weapon } }))}>Give or Sell</Button>
+
               <Button onClick={() => dispatch(setSlideOver({ type: SlideOverTypes.editWeaponForm, id: weapon._id, data: { sheetType: sheetType } }))}>Edit</Button>
               <Button
-                alert
                 onClick={() =>
                   dispatch(
-                    setModal({
-                      type: ModalTypes.deleteResource,
-                      id: weapon._id,
-                      data: {
-                        sheetType: sheetType,
-                        resourceType: 'weapons',
-                        title: `Are you sure you want to delete ${weapon.nickname || weapon.name}?`,
-                        submitText: `Yes, delete ${weapon.nickname || weapon.name}`,
-                        equipped: weapon.equipped,
-                        notification: { heading: 'Weapon Deleted', message: `You have successfully deleted ${weapon.name}.` },
-                      },
-                    })
+                    updateSheetResourceStart(
+                      sheetType,
+                      sheetType === 'characters' ? charSheet._id : campSheet._id,
+                      'weapons',
+                      weapon._id,
+                      { archived: !weapon.archived, equipped: false, active: false },
+                      {
+                        notification: {
+                          status: 'success',
+                          heading: `Weapon ${weapon.archived ? 'Restored' : 'Archived'}`,
+                          message: `You have successfully ${weapon.archived ? 'restored' : 'archived'} ${weapon.nickname || weapon.name}.`,
+                        },
+                      }
+                    )
                   )
                 }
               >
-                Delete
+                {weapon.archived ? 'Restore' : 'Archive'}
               </Button>
+              {weapon.archived ? (
+                <Button
+                  alert
+                  onClick={() =>
+                    dispatch(
+                      setModal({
+                        type: ModalTypes.deleteResource,
+                        id: weapon._id,
+                        data: {
+                          sheetType: sheetType,
+                          resourceType: 'weapons',
+                          title: `Are you sure you want to delete ${weapon.nickname || weapon.name}?`,
+                          submitText: `Yes, delete ${weapon.nickname || weapon.name}`,
+                          equipped: weapon.equipped,
+                          notification: { heading: 'Weapon Deleted', message: `You have successfully deleted ${weapon.name}.` },
+                        },
+                      })
+                    )
+                  }
+                >
+                  Delete
+                </Button>
+              ) : null}
             </div>
           </div>
         ) : (
